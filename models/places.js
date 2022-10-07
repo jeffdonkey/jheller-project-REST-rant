01e03a771cdc80dd215830d49c7b1edc8f1d6528
jeffdonkey <8jefa8@gmail.com>
2022-10-07 11:58:17 -0400

    changes in show page

--- models/places.js
+++ models/places.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+//Mongoose Schema for resturant listing
 const placeSchema = new mongoose.Schema({
     name: {type: String, required: true},
     pic: String,
@@ -8,5 +9,13 @@ const placeSchema = new mongoose.Schema({
     state: {type: String, default: 'USA'},
     founded: Number
 })
+
+//custom method built, it access the resturant's fields
+//to create a printed statement for the user
+//note that arrow functions are not allowed in this syntax
+//notice the return statement uses backticks (``)
+placeSchema.methods.showEstablished = function () {
+return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}`
+}
 
 module.exports = mongoose.model ('Place', placeSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,29 +4,43 @@ const Def = require('../default')
 // look in your notes in this project
 // there is some starter code for part 6.3
 
-function show (data) {
+function show(data) {
     return (
         <Def>
             <main>
-                <h1>{ data.place.name }</h1>
-                <img src={data.place.pic} alt={data.place.name} />
-                <div>
-                    <h4>Rating</h4>
-                    <p>No Rating</p>
-                </div>
-                <div>
-                    <h4>Comments</h4>
-                    <p>No Comments</p>
-                </div>
-                <div>
-                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                        Edit
-                    </a>
-                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                        <button type="submit" className="btn btn-danger">
-                            Delete
-                        </button>
-                    </form>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className="col-sm-6">
+                        <h1>{data.place.name}</h1>
+                        <h2>
+                            Rating
+                        </h2>
+                        No rating yet
+                        <br />
+                        <h2>
+                            Description
+                        </h2>
+                        <h5>
+                            {data.place.showEstablished()}
+                        </h5>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <br />
+                        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+                            Edit
+                        </a>{` `}
+                        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </main>
         </Def>
