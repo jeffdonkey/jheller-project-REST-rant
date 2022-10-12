1992f9585c97d52922b1d5b149742a316ec760c0
jeffdonkey <8jefa8@gmail.com>
2022-10-12 10:17:37 -0400

    part 11.1 done

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -10,7 +10,22 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated!
+        </h3>
+    )
+
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce ((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating =(
+            <h3>
+                {Math.round (averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -40,7 +55,7 @@ function show(data) {
                 <h2>
                     Rating
                 </h2>
-                No rating yet
+                {rating}
                 <br />
                 <h2>
                     Description
